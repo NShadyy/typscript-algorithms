c77c2a07bfbe0590bf2965a8388dcff3646be8fe
Nakul Sharma <56245793+nakulsharma-tal@users.noreply.github.com>
2020-08-09 14:12:26 +0530

    Add: LinkedListNode with unit tests

---
+++ src/data-structures/linked-list/LinkedListNode.ts
@@ -0,0 +1,10 @@
+export class LinkedListNode {
+  constructor(public value: any, public next: LinkedListNode = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  toString(callback?: (value: any) => string): string {
+    return callback ? callback(this.value) : `${this.value}`;
+  }
+}
